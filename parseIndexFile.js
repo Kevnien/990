45105f5c34c491d01415999f4f99fa747ebe7a1f
Kevin Nguyen <kevinn858@gmail.com>
2021-01-09 21:12:04 -0800

    finished preliminary parsing

--- parseIndexFile.js
+++ parseIndexFile.js
@@ -39,14 +39,37 @@ fetch(
     
 parse2012v2d1 = (xmlDoc) => {
     parseString(xmlDoc, (err, result) => {
-        const officerData = result.Return.ReturnHeader[0].Officer[0];
-        const form990Ez = result.Return.ReturnData[0].IRS990EZ[0];
+        const returnHeader = result.Return.ReturnHeader[0];
+        const officerData = returnHeader.Officer[0];
+        const returnData = result.Return.ReturnData[0];
+        const form990Ez = returnData.IRS990EZ[0];
         data.officerTitle = officerData.Title[0];
         data.officerName = officerData.Name[0];
         const keyEmployeesArray = form990Ez.OfficerDirectorTrusteeKeyEmpl;
         data.highestPaidEmployeeCompensation = form990Ez.PartVIOfCompOfHighestPaidEmpl[0];
         data.relatedOrgControlledEntity = form990Ez.RelatedOrgControlledEntity[0];
-        data.preparerFirm = result.Return.ReturnHeader[0].PreparerFirm[0].PreparerFirmBusinessName[0].BusinessNameLine1[0];
+        data.preparerFirm = returnHeader.PreparerFirm[0].PreparerFirmBusinessName[0].BusinessNameLine1[0];
+        data.relatedOrgControlledEntity = form990Ez.RelatedOrgControlledEntity[0];
+        data.description = form990Ez.ProgramServiceAccomplishment[0].DescriptionProgramServiceAccom[0];
+        const totalAssets = form990Ez.TotalAssets[0];
+        data.totalAssetsBOY = totalAssets.BOY[0];
+        data.totalAssetsEOY = totalAssets.EOY[0];
+        const sumOfTotalLiabilities = form990Ez.SumOfTotalLiabilities[0];
+        data.liabilitiesBOY = sumOfTotalLiabilities.BOY[0];
+        data.liabilitiesEOY = sumOfTotalLiabilities.EOY[0];
+        data.expenses = form990Ez.TotalExpenses[0];
+        data.revenue = form990Ez.TotalRevenue[0];
+        data.taxPeriodBeginDate = returnHeader.TaxPeriodBeginDate[0];
+        data.taxPeriodEndDate = returnHeader.TaxPeriodEndDate[0];
+        data.taxYear = returnHeader.TaxYear[0];
+        data.businessName = returnHeader.PreparerFirm[0].PreparerFirmBusinessName[0].BusinessNameLine1[0];
+        const filer = returnHeader.Filer[0];
+        const address = filer.USAddress[0]
+        data.addressLine1 = address.AddressLine1[0];
+        data.city = address.City[0];
+        data.state = address.State[0];
+        data.zipCode = address.ZIPCode[0];
+        data.formType = form990Ez.$.documentId;
         // console.log({ employeesArray: keyEmployeesArray.map(emp => {
         //     return {
         //         name: emp.PersonName[0],
@@ -76,7 +99,7 @@ parse2012v2d1 = (xmlDoc) => {
         if (err) return console.log(err);
     });
 
-    console.log(xmlDoc);
+    // console.log(xmlDoc);
     // console.log();
     // console.log({ data });
     // console.log();
